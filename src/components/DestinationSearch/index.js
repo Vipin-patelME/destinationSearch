581525921a7c7ad8e63a2a077a66dd2b89311455
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-09-10 17:39:28 +0530

    Update index.js

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,18 +1,42 @@
-import Placelist from './component/DestinationItem'
-import {component} from 'react'
+import {Component} from 'react'
+import Placelist from '../DestinationItem'
 import './index.css'
 
-class DestinationSearch extends component {
+class DestinationSearch extends Component {
+  state = {searchinput: ''}
+
+  userSearcInput = event => {
+    this.setState({
+      searchinput: event.target.value,
+    })
+  }
+
   render() {
     const {destinationsList} = this.props
+    const {searchinput} = this.state
+    console.log(searchinput)
+    const serchedlist = destinationsList.filter(eachUser =>
+      eachUser.name.includes(searchinput),
+    )
 
     return (
       <div className="bg-container">
         <h1 className="heading">Destination Search</h1>
-        <input type="search" onChange={this.userSearcInput} />
+        <div className="input-cont">
+          <input
+            value={searchinput}
+            type="search"
+            onChange={this.userSearcInput}
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+            className="searchIcon"
+          />
+        </div>
         <ul className="list-container">
-          {destinationsList.map(eachPlace => (
-            <Placelist eachPlace={eachPlace} />
+          {serchedlist.map(eachPlace => (
+            <Placelist eachPlace={eachPlace} key={eachPlace.id} />
           ))}
         </ul>
       </div>
